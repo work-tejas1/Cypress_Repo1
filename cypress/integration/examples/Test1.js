8b203eec9ae3fd01d242ac8a72a78f8734cfeb5a
Tejas <work.tejas1@gmail.com>
2021-04-03 06:11:29 +0530

    pageObject implememtation test

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,23 +1,24 @@
 /// <reference types="cypress" />
 
-describe('My First Suit', function() 
-{
-    it('My First Test Case', function() {
+describe('My First Suit', function () {
+    it('My First Test Case', function () {
         cy.visit('https://unikrn.com/')
         Cypress.on('uncaught:exception', (err, runnable) => {
             // returning false here prevents Cypress from
             // failing the test
             return false
-          })
-          it('fails to visit website 1', function () {
+        })
+        it('fails to visit website 1', function () {
             cy.visit('https://unikrn.com/')
-          })
-
-
-          cy.get("[type='button']").eq(4).click()
-          //cy.get('[data-test=LoginSignupLoginButton] > .translation--as-html').click()
-          //cy.get("[type='submit']").click() // Click on first el containing 'Welcome'
+           // cy.visit('https://admin-demo.nopcommerce.com/')
+           
+        })
+        //cy.get("[type='button']").eq(4).click() //clicking on "START PLAYING"
+        //cy.get('[data-test=LoginSignupLoginButton] > .translation--as-html').click()
+        
+        const login = cy.get('[type=button]')
+        login.click
         //https://github.com/cypress-io/cypress/issues/2554 issue is closed. Talk to Peere
     })
 
-  })
+})
