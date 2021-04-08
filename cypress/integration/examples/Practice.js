15478033180f08dbfcef5b2df11d9ad1545d3da4
Tejas <work.tejas1@gmail.com>
2021-04-09 04:01:29 +0530

    iterating through every element using loop (text)

--- cypress/integration/examples/Practice.js
+++ cypress/integration/examples/Practice.js
@@ -15,23 +15,34 @@ describe('My First Suit', function () {
         //cy.get("[type='button']").eq(4).click() //clicking on "START PLAYING"
         //cy.get('[data-test=LoginSignupLoginButton] > .translation--as-html').click()
         cy.get('.search-keyword').type('ca')
-       // cy.wait(2000)
+       //cy.wait(2000)
 
-        //only visible 
+        //only visible element
         cy.get('.product:visible').should('have.length',4)
         
         //Parent child chaining (contrain element search to only particular block)
         cy.get('.products').find('.product').should('have.length',4)
 
         //click on add to cart
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        // cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+        //find text and then click 
+        cy.get('.products').find('.product').each(($element, index, $list) => {
+
+        const nameVeg = $element.find('h4.product-name').text()
+        if(nameVeg.includes('Capsicum'))    //click on Capsicum
+        {
+        $element.find('button').click()
+        }
+
+        })
+          
+    
 
 
 
 
 
-       
-     
     })
 
 })
