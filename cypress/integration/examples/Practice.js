650e63cccc05cca3fdfea63ad6bf63bf53ee63d5
Tejas <work.tejas1@gmail.com>
2021-04-09 05:14:49 +0530

    ALIASING and ASSERT commends demo

--- cypress/integration/examples/Practice.js
+++ cypress/integration/examples/Practice.js
@@ -3,38 +3,34 @@
 describe('My First Suit', function () {
     it('My First Test Case', function () {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
-        // Cypress.on('uncaught:exception', (err, runnable) => {
-        //     returning false here prevents Cypress from
-        //     failing the test
-        //     return false
-        // })
-        // it('fails to visit website 1', function () {
-        //     cy.visit('https://unikrn.com/')
-        //    cy.visit('https://admin-demo.nopcommerce.com/')
-        // })
-        //cy.get("[type='button']").eq(4).click() //clicking on "START PLAYING"
-        //cy.get('[data-test=LoginSignupLoginButton] > .translation--as-html').click()
+  
         cy.get('.search-keyword').type('ca')
        //cy.wait(2000)
+
+       //ALIASING
+       cy.get('.products').as('prodLocator')
 
         //only visible element
         cy.get('.product:visible').should('have.length',4)
         
-        //Parent child chaining (contrain element search to only particular block)
-        cy.get('.products').find('.product').should('have.length',4)
+        //Parent child chaining (contrain element search to only PARTICULAR block)
+        cy.get('@prodLocator').find('.product').should('have.length',4)
 
         //click on add to cart
         // cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
 
-        //find text and then click (using loop to iterare each element) 
-        cy.get('.products').find('.product').each(($element, index, $list) => {
+        //find text and then click (using loop to iterare EACH  element) 
+        cy.get('@prodLocator').find('.product').each(($element, index, $list) => {
         const nameVeg = $element.find('h4.product-name').text()
-        if(nameVeg.includes('Carrot'))    //click on Capsicum
+        if(nameVeg.includes('Capsicum'))    //click on Capsicum
         {
         $element.find('button').click()
         }
         })
-          
+        
+        //ASSERT 
+        cy.get('.brand').should('have.text','GREENKART11')
+
         //understanding THEN function due to asynchronous nature of cypress
         cy.get('.brand').then(function(logoElement)
         {
