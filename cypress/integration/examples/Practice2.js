6c983e6ee3450f0573125f5f91baa423a7d1c3d0
Tejas <work.tejas1@gmail.com>
2021-04-10 04:26:02 +0530

    static dropdown

--- cypress/integration/examples/Practice2.js
+++ cypress/integration/examples/Practice2.js
@@ -8,10 +8,13 @@ describe('My Second Suit', function () {
         //cy.get('#checkBoxOption1').first().check() // can use this
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked').and('have.value','option1') // opposite
-        
-        //Multiple checkboxes
-        cy.get('input[type="checkbox"]').check(['option2','option3']) //check method will accept value property
+            //Multiple checkboxes
+            cy.get('input[type="checkbox"]').check(['option2','option3']) //check method will accept value property
     
+        //Static Dropdown
+        cy.get('select').select('option3').should('have.value','option3')
+        
+        
 
     })
 
