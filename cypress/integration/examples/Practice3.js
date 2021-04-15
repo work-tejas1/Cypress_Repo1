a4c4801d5d7e4fda1d3a0a5993ec1a8b46b81f5f
Tejas <work.tejas1@gmail.com>
2021-04-16 01:47:52 +0200

    handilng new windows using removeAttr

--- cypress/integration/examples/Practice3.js
+++ cypress/integration/examples/Practice3.js
@@ -1,21 +1,15 @@
 /// <reference types="cypress" />
 
-describe('My Third Suit', function () {
-    it('My Third Test Case', function () {
+describe('My 4th Suit', function () {
+    it('My 4th Test Case', function () {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        
+        //Child window handling
+        cy.get('#opentab').invoke('removeAttr','target').click()
 
-        //Handling Alerts
-        cy.get('#alertbtn').click()
-        cy.get('[value="Confirm"]').click()
 
-        //Fire event on browser/ manupulate DOM
-        cy.on('window:alert',(str) =>
-        {
-            expect(str).to.equals('Hello , share this practice page and share your knowledge')
 
-        })
-
-    
+        
     })
 
 })
---
+++ cypress/integration/examples/Practice4.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('My Third Suit', function () {
+    it('My Third Test Case', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        //Handling Alerts
+        cy.get('#alertbtn').click()
+        cy.get('[value="Confirm"]').click()
+
+        //Fire event on browser/ manupulate DOM
+        cy.on('window:alert',(str) =>
+        {
+            expect(str).to.equals('Hello , share this practice page and share your knowledge')
+
+        })
+
+    
+    })
+
+})
