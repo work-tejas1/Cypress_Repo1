7db2dd336a4b68b6943dd2636b821f7172ec8c96
Tejas <work.tejas1@gmail.com>
2021-04-16 02:44:45 +0200

    complex x-path self practice

---
+++ cypress/integration/examples/Practice5_complexCSS.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('My Third Suit', function () {
+    it('My Third Test Case', function () {
+        cy.visit('https://www.cricbuzz.com/cricket-series/3472/indian-premier-league-2021/points-table')
+
+       cy.get('table tr td:nth-child(7)').each(($element, index, $list) => {
+       const points = $element.text()
+       cy.log(points)
+       })
+    })
+
+})
