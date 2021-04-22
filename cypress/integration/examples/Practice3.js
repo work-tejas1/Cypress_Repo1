f24eec90f2af81c6d180a0ffe82ffb9f9d259947
Tejas <work.tejas1@gmail.com>
2021-04-23 01:57:16 +0200

    handling frames

--- cypress/integration/examples/Practice3.js
+++ cypress/integration/examples/Practice3.js
@@ -8,13 +8,22 @@ describe('My 4th Suit', function () {
         cy.get('#opentab').invoke('removeAttr','target').click()
         
         //get URL and compare
-        cy.url().should('include','rahulshetty')     // compare partial string
+       // cy.url().should('include','rahulshetty')     // compare partial string
         //browser navigation
         cy.go('back')
-        
-
-
-
     })
+
+    it.only('My 4(1)th Test Case', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        
+        //Child window handling
+        cy.get('#opentab').then(function(el)
+        {
+            const url = el.prop('href')
+            cy.log(url)
+            cy.visit(url)
+        })
+    })
+
 
 })
---
+++ cypress/integration/examples/Practice7_frames.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('My 7th Suit', function () {
+    it('My 7th Test Case', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        
+  
+
+    })
+
+})
